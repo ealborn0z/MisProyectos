ee21a6e6e26056242372270999cd5eafe90b679e
ealborn0z <108744609+ealborn0z@users.noreply.github.com>
2023-12-17 21:24:11 -0400

    Delete Angularpro/Web-PresentationAngular directory

--- Angularpro/Web-PresentationAngular/src/main.ts
+++
@@ -1,8 +0,0 @@
-import 'aos/dist/aos.css'; // Importa los estilos CSS de AOS
-import AOS from 'aos';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-AOS.init(); // Inicializa AOS
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
